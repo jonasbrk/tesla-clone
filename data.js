54ad1e7071a0193e5c220c32da243f38d1953f3a
João Pedro <jonas0378@gmail.com>
2022-01-20 21:40:56 -0300

    add features/fix bugs/refactoring code part 2

--- data.js
+++ data.js
@@ -1,6 +1,8 @@
 
 
-const Models = [
+
+
+const modelsData = [
 
     {
         model: "Model 3",
@@ -8,7 +10,8 @@ const Models = [
         price: "39,940",
         autonomy: "267",
         topSpeed: "140",
-        imageUrl: "",
+        imageBg: "assets/model-3-bg.jpg",
+        imageBgMobile: "assets/model-3-bg-m.jpg",
 
     },
     {
@@ -17,7 +20,8 @@ const Models = [
         price: "53,940",
         autonomy: "318",
         topSpeed: "135",
-        imageUrl: "",
+        imageBg: "assets/model-y-bg.jpg",
+        imageBgMobile: "assets/model-y-bg-m.jpg",
 
     },
     {
@@ -26,34 +30,41 @@ const Models = [
         price: "88,740",
         autonomy: "375",
         topSpeed: "155",
-        imageUrl: "",
+        imageBg: "assets/model-s-bg.jpg",
+        imageBgMobile: "assets/model-s-bg-m.jpg",
 
     },
     {
         model: "Model X",
-        id: "model-X",
+        id: "model-x",
         price: "98,940",
         autonomy: "332",
         topSpeed: "155",
-        imageUrl: "",
+        imageBg: "assets/model-x-bg.jpg",
+        imageBgMobile: "assets/model-x-bg-m.jpg",
 
     },
     {
         model: "Solar Panels",
         id: "solar-panels",
-        imageUrl: "",
+        imageBg: "assets/solar-panel-bg.jpg",
+        imageBgMobile: "assets/solar-panel-bg-m.jpg",
 
     },
     {
         model: "Solar Roof",
         id: "solar-roof",
-        imageUrl: "",
+        imageBg: "assets/solar-roof-bg.jpg",
+        imageBgMobile: "assets/solar-roof-bg-m.jpg",
 
     },
     {
         model: "Accessories",
         id: "accessories",
-        imageUrl: "",
+        imageBg: "assets/acessories-bg.jpg",
+        imageBgMobile: "assets/acessories-bg-m.jpg",
 
     },
 ]
+
+export default modelsData;
--- style.js
+++ style.js
@@ -1,16 +1,46 @@
 const marker = document.querySelector('#marker');
-const item = document.querySelectorAll('.container-central-menu a, .container-menu a');
 const header = document.querySelector('header');
 const main = document.querySelector('main');
 const body = document.querySelector('body');
+const headerCentral = document.querySelector('.header-wrapper .container-central-menu')
+import modelsData from "./data.js";
 // const model = document.querySelectorAll('#models div');
-
-
-
-
 const easeInOutCubic = (t) => {
     return t < .5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2
 };
+
+
+
+
+///////////////ELEMENTS GENERATOR///////////////
+
+modelsData.forEach((element, index, array) => {
+
+    let section = document.createElement('section');
+    let a = document.createElement('a');
+
+    /////////MODEL SECTIONS/////////
+    section.innerHTML = `
+            <picture>
+                <source media="(max-width: 911px)" srcset="${modelsData[index].imageBgMobile}">
+                <img src="${modelsData[index].imageBg}">
+            </picture>
+        `;
+    section.classList.add('section-model')
+    section.setAttribute("id", modelsData[index].id)
+    main.appendChild(section)
+
+    /////////CENTRAL MENU/////////
+
+    a.innerHTML = `<span>${modelsData[index].model}</span>`
+    a.setAttribute('href', `#${modelsData[index].id}`)
+    headerCentral.appendChild(a)
+
+})
+
+const item = document.querySelectorAll('.container-central-menu a, .container-menu a');
+
+
 
 
 
@@ -22,9 +52,9 @@ function indicator(e) {
 
     marker.style.left = e.offsetLeft - 12 + 'px';
     marker.style.width = e.offsetWidth + 'px';
-    marker.style.transition = 'opacity 1.5s , width 0.5s';
+    marker.style.transition = 'opacity 1s , width 0.5s';
     marker.style.opacity = '1';
-    marker.style.transition = 'opacity 1.5s , width 0.5s, left 0.5s';
+    marker.style.transition = 'opacity 1s , width 0.5s, left 0.5s';
 
 
 }
@@ -41,22 +71,52 @@ item.forEach(link => {
 
 header.addEventListener('mouseleave', () => {
 
-    marker.style.transition = 'opacity 1.5s , width 0.5s';
+    marker.style.transition = 'opacity 1s , width 0.5s';
     marker.style.opacity = '0';
-    marker.style.transition = 'opacity 1.5s , width 0.5s, left 0s ';
+    marker.style.transition = 'opacity 1s , width 0.5s, left 0s ';
 
 })
 
+
 ///////////////SCROLL ANIMATIONS///////////////
 
-main.addEventListener('scroll', scrollAnimation());
+main.addEventListener('scroll', () => {
 
-function scrollAnimation() {
-    const
+    let model = document.querySelectorAll('.section-model')
+    let modelReveal = document.querySelector('#model')
+    let sectionScrollValue = model[0].getBoundingClientRect().top / window.innerHeight;
+    let sectionIndex = Math.abs(sectionScrollValue.toFixed(0))
+    let scrollOpacity = Math.abs(1 - ((Math.abs(sectionScrollValue.toFixed(2)) % 1) * 2));
+    let modelInfo = modelsData[sectionIndex];
 
+    for (let i = 0; i < model.length; i++) {
 
-}
+        if (easeInOutCubic(scrollOpacity) > 0 && sectionIndex == i) {
+            modelReveal.style.opacity = easeInOutCubic(scrollOpacity)
+        }
+        if (easeInOutCubic(scrollOpacity) >= 0.000000 && easeInOutCubic(scrollOpacity) <= 0.01 && sectionIndex == i) {
 
+            modelReveal.children[0].innerHTML = `                
+    
+            <div class="model-wrapper">
+                <h2 class="model-title">${modelInfo.model}</h2>
+                <h3 class="model-description">Order Online for <a>Touchless Delivery</a></h3>
+            </div>
+            <div class="button-wrapper">
+                <a href="#">CUSTOM ORDER</a>
+                <a href="#">EXISTING INVENTORY</a>
+             </div>`
+
+            let iniAnimationItens = document.querySelectorAll('header, .model-title, .model-description, .button-wrapper a:nth-child(1), .button-wrapper a:nth-child(2)')
+
+            console.log(iniAnimationItens)
+            iniAnimationItens.forEach(item => {
+
+                item.classList.add('animation-none')
+            });
+        }
+    }
+})
 
 
 
@@ -88,11 +148,9 @@ function scrollAnimation() {
     let screenopacitycalc = Math.abs(screenindex.toFixed(2)) % 1;
     let screenopacity = Math.abs(1 - (screenopacitycalc.toFixed(2) * 2))
 
-
     for (let i = 0; i < model.length; i++) {
 
         if (easeInOutCubic(screenopacity) > 0 && i == Math.abs(Math.round(screenindex))) {
-
 
             var revealsIndex = i
             reveals[Math.abs(Math.round(screenindex))].children[0].style.display = "flex"
@@ -101,8 +159,6 @@ function scrollAnimation() {
             reveals[Math.abs(Math.round(screenindex))].children[1].style.opacity = easeInOutCubic(screenopacity);
 
         }
-
-
         if (revealsIndex != Math.abs(Math.round(screenindex)) && revealsIndex != Math.abs(Math.round(screenindex)) + 1) {
 
             for (let i = 0; i < model.length; i++) {
